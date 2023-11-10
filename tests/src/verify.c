e2af7efcf45cbec4956d466e4075dde9679d306f
Krishnan Iyer <krishnaniyer97@gmail.com>
2023-11-11 01:57:27 +0300

    generalise verify

--- tests/src/verify.c
+++ tests/src/verify.c
@@ -14,45 +14,65 @@ uint64_t twos_compliment(int64_t num){
 
 int main (int argc, char* argv[]) {
 
-    int32_t s_op1,s_op2, t_op1, t_op2;
-    int64_t s_dst, t_dst;
-
-    FILE *sim_out, *true_out;
 
     if(argc == 2){
         if(strcmp(argv[1],"-h") == 0){
-            printf("Usage           - ./verify sim_out.txt true_out.txt \n");
+            printf("Usage           - ./verify 3 sim_out.txt true_out.txt \n");
             printf("File Format     - op1 op2 rst\\n\n");
             printf("sim_out.txt     - Simulation results\n");
             printf("true_out.txt    - True output\n");
         }
         return 0;
     }
-    else if(argc != 3){
+    else if(argc != 4){
         printf("Need atleast two arguments or --h \r\n");
         return 0;
     }
 
-    
-    sim_out  = fopen(argv[1], "r");
-    true_out = fopen(argv[2], "r");
+
+    int row = atoi(argv[1]);
+    int32_t s_in[row], t_in[row];
+    int64_t s_dst, t_dst;
+
+    FILE *sim_out, *true_out;
+
+
+    sim_out  = fopen(argv[2], "r");
+    true_out = fopen(argv[3], "r");
     
     uint8_t success = 1;
+    uint64_t count_test = 0;
 
     while (!feof(sim_out)){
 
-        fscanf(sim_out,"%d %d %ld\n",&s_op1,&s_op2,&s_dst);
-        fscanf(true_out,"%d %d %ld\n",&t_op1,&t_op2,&t_dst);
-
-        if(s_op1 != t_op1 || s_op2 != t_op2 || s_dst != t_dst){
-            printf("Verification failed --- sim : %d %d %ld ; \
-                true : %d %d %ld \r\n", s_op1,s_op2,s_dst,t_op1,t_op2,t_dst);
-            success = 0;
-            break;
+        for(int i=0;i<row;i++){
+            fscanf(sim_out,"%d",&s_in[i]);
+            fscanf(true_out,"%d",&t_in[i]);
         }
+        fscanf(sim_out,"\n");
+        fscanf(true_out,"\n");
+    
+        for(int i=0;i<row;i++){
+            if(s_in[i] != t_in[i]){
+                printf("Verification failed (success %ld) --- sim : ",count_test);
+                for (int j = 0; j < row; j++)
+                {
+                    printf("%d ",s_in[j]);
+                }
+                printf("true: ");
+                for (int j = 0; j < row; j++)
+                {
+                    printf("%d ",t_in[j]);
+                }
+                printf("\n");                
+                success = 0;
+                break;
+            }
+        }
+        count_test += 1;
     }
     if(success)
-        printf("Succeeded\n");
+        printf("Succeeded tests : %ld\n",count_test);
 
     fclose(sim_out);
     fclose(true_out);
